509801b8524d78972d8ea92e0902fa53d1f5ed33
Ilya Sheprut <optitel223@gmail.com>
2020-01-25 02:29:00 +0700

    draw text in raqote by rusttype

--- examples/raqote.rs
+++ examples/raqote.rs
@@ -1,10 +1,8 @@
 use bufdraw::*;
 use bufdraw::vec::*;
+use bufdraw::image::*;
 
 use raqote::*;
-use font_kit::source::SystemSource;
-use font_kit::family_name::FamilyName;
-use font_kit::properties::Properties;
 
 use log::info;
 
@@ -55,7 +53,7 @@ impl MyEvents for Window {
 
         let mut pb = PathBuilder::new();
         pb.move_to(100., 10.);
-        pb.cubic_to(150., 40., 175., 0., 200., 10.);
+        pb.cubic_to(150., 40., 175., self.counter as f32, 200., 10.);
         pb.quad_to(120., 100., 80., 200.);
         pb.quad_to(150., 180., 300., 300.);
         pb.close();
@@ -109,24 +107,12 @@ impl MyEvents for Window {
             &DrawOptions::new()
         );
 
-        let font = SystemSource::new()
-            .select_best_match(&[FamilyName::SansSerif], &Properties::new())
-            .unwrap()
-            .load()
-            .unwrap();
-
-        dt.draw_text(
-            &font,
-            24.,
-            "Hello",
-            Point::new(0., 100.),
-            &Source::Solid(SolidSource {
-                r: 0,
-                g: 0,
-                b: 0xff,
-                a: 0xff,
-            }),
-            &DrawOptions::new(),
+        draw_text(
+            dt, 
+            format!("Русский текст ± ≈ ≠ × ÷ ∃ ∀ {} ∞", self.counter).as_str(), 
+            32.0, 
+            &Vec2i::new(50, 100), 
+            &bufdraw::image::Color::rgba(0, 0, 150, 128)
         );
     }
 
@@ -146,6 +132,84 @@ impl MyEvents for Window {
 
     fn mouse_wheel_event(&mut self, pos: Vec2i, _dir: MouseWheel, _press: bool) {
         self.wheel = pos;
+    }
+}
+
+use rusttype::{point, Font, Scale};
+
+fn blend(up: &bufdraw::image::Color, low: &bufdraw::image::Color) -> bufdraw::image::Color {
+    let upr:i32 = up.r as i32;
+    let upg:i32 = up.g as i32;
+    let upb:i32 = up.b as i32;
+    let upa:i32 = up.a as i32;
+
+    let lowr:i32 = low.r as i32;
+    let lowg:i32 = low.g as i32;
+    let lowb:i32 = low.b as i32;
+    let lowa:i32 = low.a as i32;
+
+    bufdraw::image::Color::rgba(
+        (((upr - lowr) * upa + (lowr << 8)) >> 8) as u8,
+        (((upg - lowg) * upa + (lowg << 8)) >> 8) as u8,
+        (((upb - lowb) * upa + (lowb << 8)) >> 8) as u8,
+        ((upa + lowa) - ((lowa * upa + 255) >> 8)) as u8
+    )
+}
+
+// Modified https://github.com/redox-os/rusttype/blob/master/dev/examples/image.rs
+fn draw_text(image: &mut DrawTarget, text: &str, size: f32, pos: &Vec2i, color: &bufdraw::image::Color) {
+    // Load the font
+    let font_data = include_bytes!("WenQuanYiMicroHei.ttf");
+    // This only succeeds if collection consists of one font
+    let font = Font::from_bytes(font_data as &[u8]).expect("Error constructing Font");
+
+    // The font size to use
+    let scale = Scale::uniform(size);
+
+    let v_metrics = font.v_metrics(scale);
+
+    // layout the glyphs in a line with 20 pixels padding
+    let glyphs: Vec<_> = font
+        .layout(text, scale, point(20.0, 20.0 + v_metrics.ascent))
+        .collect();
+
+    // work out the layout size
+    let glyphs_height = (v_metrics.ascent - v_metrics.descent).ceil() as u32;
+    let glyphs_width = {
+        let min_x = glyphs
+            .first()
+            .map(|g| g.pixel_bounding_box().unwrap().min.x)
+            .unwrap();
+        let max_x = glyphs
+            .last()
+            .map(|g| g.pixel_bounding_box().unwrap().max.x)
+            .unwrap();
+        (max_x - min_x) as u32
+    };
+
+    // Loop through the glyphs in the text, positing each one on a line
+    for glyph in glyphs {
+        if let Some(bounding_box) = glyph.pixel_bounding_box() {
+            // Draw the glyph into the image per-pixel by using the draw closure
+            glyph.draw(|x, y, v| {
+                let x = (x as i32 + pos.x) as u32 + bounding_box.min.x as u32;
+                let y = (y as i32 + pos.y) as u32 + bounding_box.min.y as u32;
+                let a = (v * (color.a as f32)) as u8;
+                let c = bufdraw::image::Color::rgba(color.b, color.g, color.r, a);
+                let pos = x as usize + y as usize * image.width() as usize;
+                if x >= 0 && (x as i32) < image.width() && y >= 0 && (y as i32) < image.height() {
+                    if let Some(elem) = image.get_data_mut().get_mut(pos) {
+                        //(A << 24) | (R << 16) | (G << 8) | B
+                        let bytes: [u8; 4] = (*elem).to_be_bytes();
+                        let current_color = bufdraw::image::Color::rgba(bytes[1], bytes[2], bytes[3], bytes[0]);
+                        let new_color = blend(&c, &current_color);
+                        *elem = u32::from_be_bytes([new_color.a, new_color.r, new_color.g, new_color.b]);
+                    }    
+                }
+                
+                    
+            });
+        }
     }
 }
 
